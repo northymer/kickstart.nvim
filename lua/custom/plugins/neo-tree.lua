0832648220151c0cb31e5b5c30293133354c642f
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-03-28 20:32:05 +0300

    feat: neo-tree config start

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,4 +7,14 @@ return {
     'MunifTanjim/nui.nvim',
     '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    }
+  end,
 }
