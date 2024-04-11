762b815a2e08f4f5688afcee950ae5a10c79431f
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-04-11 13:56:03 +0300

    feat: eslint_d in null-ls

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,3 +1,12 @@
 return {
   'nvimtools/none-ls.nvim',
+  requires = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.eslint_d,
+      },
+    }
+  end,
 }
