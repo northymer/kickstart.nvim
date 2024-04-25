d7fa0c925340ae15ecf7a6d62a75c08f2222b109
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-04-25 22:11:53 +0300

    feat: null-ls tweaks

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,10 +1,27 @@
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 return {
   'nvimtools/none-ls.nvim',
-  requires = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local null_ls = require 'null-ls'
     null_ls.setup {
+      on_attach = function(client, bufnr)
+        if client.supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format { async = false }
+              -- vim.lsp.buf.formatting_sync()
+            end,
+          })
+        end
+      end,
       sources = {
+        null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.eslint_d,
       },
     }
