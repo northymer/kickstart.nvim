6ae4d00a5cc4732f1893f8c8956635ad315b0d8e
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-07-21 21:32:30 +0300

    feat: conform tweaks

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -9,32 +9,17 @@ return { -- Autoformat
       },
     },
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
+    format_on_save = {
+      lsp_format = 'fallback',
+      timeout_ms = 500,
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- typescript = {},
-      -- typescriptreact = {},
-      -- javascript = {},
-      -- javascriptreact = {},
-      typescript = { { 'prettierd', 'prettier' } },
-      typescriptreact = { { 'prettierd', 'prettier' } },
-      javascript = { { 'prettierd', 'prettier' } },
-      javascriptreact = { { 'prettierd', 'prettier' } },
-      css = { { 'stylelint' } },
+      typescript = { 'eslint', 'prettierd' },
+      typescriptreact = { 'eslint', 'prettierd' },
+      javascript = { 'eslint', 'prettierd' },
+      javascriptreact = { 'eslint', 'prettierd' },
+      css = { 'stylelint' },
     },
   },
 }
