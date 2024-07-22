4a04e0a1de0f88ea1b069d9b797c9eb8669f0897
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-07-22 16:50:35 +0300

    feat: typescript tools shenanigans

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -6,6 +6,12 @@ return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {
+    separate_diagnostic_server = true,
+    publish_diagnostic_on = 'insert_leave',
     tsserver_max_memory = 'auto',
+    jsx_close_tag = {
+      enabled = true,
+      filetypes = { 'javascriptreact', 'typescriptreact' },
+    },
   },
 }
