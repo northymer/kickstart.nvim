93b7672b18197c9ce08d00870ab49d77cc0b9aae
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-07-22 20:46:30 +0300

    feat: remove some opts

--- lua/options.lua
+++ lua/options.lua
@@ -60,9 +60,4 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.bo.softtabstop = 2
-
 vim.diagnostic.config { virtual_text = false }
