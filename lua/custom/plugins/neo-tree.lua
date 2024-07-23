2df5f74f0fb702e4589d37940c527f868035c2bd
Konstantin Kirilin <cons.kirilin@gmail.com>
2024-07-23 11:19:24 +0300

    feat: neo-tree mappings

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -18,6 +18,23 @@ return {
       },
       window = {
         mappings = {
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['<space>'] = 'none',
+          ['Y'] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg('+', path, 'c')
+            end,
+            desc = 'Copy Path to Clipboard',
+          },
+          ['O'] = {
+            function(state)
+              require('lazy.util').open(state.tree:get_node().path, { system = true })
+            end,
+            desc = 'Open with System Application',
+          },
           ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = false } },
         },
       },
