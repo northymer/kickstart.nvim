e5ebe8bdebdd38ae77b49eb5636e99b72207870c
Konstantin Kirilin <cons.kirilin@gmail.com>
2025-07-01 20:52:19 +0200

    feat: health and neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,46 +1,12 @@
 -- Replacement for standart netrw view, + a decent sidebar
 return {
-  -- 'nvim-neo-tree/neo-tree.nvim',
-  -- branch = 'v3.x',
-  -- dependencies = {
-  --   'nvim-lua/plenary.nvim',
-  --   'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-  --   'MunifTanjim/nui.nvim',
-  --   -- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
-  -- },
-  -- config = function()
-  --   require('neo-tree').setup {
-  --     close_if_last_window = true,
-  --     filesystem = {
-  --       follow_current_file = {
-  --         enabled = true,
-  --       },
-  --     },
-  --     window = {
-  --       mappings = {
-  --         ['l'] = 'open',
-  --         ['h'] = 'close_node',
-  --         ['<space>'] = 'none',
-  --         ['Y'] = {
-  --           function(state)
-  --             local node = state.tree:get_node()
-  --             local path = node:get_id()
-  --             vim.fn.setreg('+', path, 'c')
-  --           end,
-  --           desc = 'Copy Path to Clipboard',
-  --         },
-  --         ['O'] = {
-  --           function(state)
-  --             require('lazy.util').open(state.tree:get_node().path, { system = true })
-  --           end,
-  --           desc = 'Open with System Application',
-  --         },
-  --         ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = false } },
-  --       },
-  --     },
-  --   }
-  -- end,
   'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
   cmd = 'Neotree',
   keys = {
     {
@@ -55,8 +21,6 @@ return {
     vim.cmd [[Neotree close]]
   end,
   init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
     vim.api.nvim_create_autocmd('BufEnter', {
       group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
       desc = 'Start Neo-tree with directory',
